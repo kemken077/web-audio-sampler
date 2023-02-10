cecca07f4b7fec0a32cb7dc5355fd0c90e4584d5
¨kemken077¨ <kemken077@gmail.com>
2023-02-10 13:06:49 -0300

    Organize code, start UI play button

--- public/js/sampler.js
+++ public/js/sampler.js
@@ -1,33 +1,40 @@
+let _audioContext;
+const samples = {};
+const playUIButton = document.getElementById('play');
+const clickEventTypeByDevice = isMobile() ? 'touchstart' : 'click';
+console.log({clickEventTypeByDevice});
+
 function isMobile() {
   return /Mobi/.test(navigator.userAgent);
 }
 
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-const clickEventTypeByDevice = isMobile() ? 'click' : 'touchstart';
-const playUIButton = document.getElementById('play');
+function playDevice() {
+  console.log('PLAYING...');
+  loadSample('amen', './public/audio/amen.wav');
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  _audioContext = audioContext;
+  // Later, you can play a sample like this:
+  playSample(samples['amen']);
+}
 
 function playSample(buffer) {
-  const source = audioContext.createBufferSource();
+  const source = _audioContext.createBufferSource();
   source.buffer = buffer;
-  source.connect(audioContext.destination);
+  source.connect(_audioContext.destination);
   source.start();
 }
 
-const samples = {};
 
 function loadSample(name, url) {
   fetch(url)
     .then(response => response.arrayBuffer())
-    .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+    .then(arrayBuffer => _audioContext.decodeAudioData(arrayBuffer))
     .then(audioBuffer => {
       samples[name] = audioBuffer;
     });
 }
 
-loadSample('amen', './public/audio/amen.wav');
-
 
 playUIButton.addEventListener(clickEventTypeByDevice, () => {
-  // Later, you can play a sample like this:
-  playSample(samples['amen']);
+  playDevice();
 });
