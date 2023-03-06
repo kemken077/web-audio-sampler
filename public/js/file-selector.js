3aca976047c84c7d7ef964d3cd43d36fcbac0916
¨kemken077¨ <kemken077@gmail.com>
2023-03-06 20:58:14 -0300

    File selection code into main JS file, keep only one file

--- public/js/file-selector.js
+++
@@ -1,8 +0,0 @@
-const fileSelector = document.getElementById('file-selector');
-const handleFileSelection = (event) => {
-  const fileList = event.target.files;
-  console.log(fileList);
-}; 
-
-
-fileSelector.addEventListener('change', handleFileSelection);
--- public/js/sampler.js
+++ public/js/sampler.js
@@ -1,4 +1,16 @@
+// File selection
+const fileSelector = document.getElementById('file-selector');
+const handleFileSelection = (event) => {
+  const fileList = event.target.files;
+  console.log(fileList);
+};
 
+
+fileSelector.addEventListener('change', handleFileSelection);
+
+
+
+// Sample
 const playButton = document.getElementById('play');
 const pauseButton = document.getElementById('pause');
 const stopButton = document.getElementById('stop');
